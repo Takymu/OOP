c79c3991c908657d376459a1395d590026e083b8
Takumy <Talumy@duck.com>
2024-09-09 19:15:08 +0700

    shell script and heapsort added

--- Task_1_1_1/src/test/java/ru/nsu/pereverzev/MainTest.java
+++ Task_1_1_1/src/test/java/ru/nsu/pereverzev/MainTest.java
@@ -8,10 +8,10 @@ import static ru.nsu.pereverzev.Main.heapsort;
 class MainTest {
     @Test
     void SampleTest() {
-        int[] arr = {1,5,1,2,9,4,7,6,3,2};
+        int[] arr = {1, 5, 1, 2, 9, 4, 7, 6, 3, 2};
         heapsort(arr);
         int len = arr.length;
-        int[] sorted = {1,1,2,2,3,4,5,6,7,9};
+        int[] sorted = {1, 1, 2, 2, 3, 4, 5, 6, 7, 9};
         for(int i = 0; i < arr.length; i++) {
             assertEquals(arr[i], sorted[i]);
         }
