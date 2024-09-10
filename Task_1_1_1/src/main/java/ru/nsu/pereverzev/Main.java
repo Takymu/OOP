ec86452df303bfb84e52053aca838cfb8ce8c282
Takumy <Talumy@duck.com>
2024-09-10 15:22:25 +0700

    another codestyle correctives

--- Task_1_1_1/src/main/java/ru/nsu/pereverzev/Main.java
+++ Task_1_1_1/src/main/java/ru/nsu/pereverzev/Main.java
@@ -3,15 +3,16 @@ package ru.nsu.pereverzev;
 public class Main {
     public static void heapsort(int[] array) {
         Heap heap = new Heap(array.length);
-        for(int i = 0; i < array.length; i++) {
+        for (int i = 0; i < array.length; i++) {
             heap.add(array[i]);
         }
-        for(int i = array.length; i > 0; i--) {
-            array[i-1] = heap.pop();
+        for (int i = array.length; i > 0; i--) {
+            array[i - 1] = heap.pop();
         }
     }
+
     public static void main(String[] args) {
-        int[] arr = {1,2,3,4};
+        int[] arr = {1, 2, 3, 4};
         heapsort(arr);
     }
 }
@@ -19,6 +20,7 @@ public class Main {
 class Heap {
     int[] heap;
     int cursize = 0;
+
     Heap(int heapsize) {
         heap = new int[heapsize];
     }
@@ -37,11 +39,9 @@ class Heap {
 
     private void sift_up() {
         int curId = cursize - 1;
-        while (curId != 0)
-        {
+        while (curId != 0) {
             int parId = (curId - 1) / 2;
-            if (heap[parId] < heap[curId])
-            {
+            if (heap[parId] < heap[curId]) {
                 int temp = heap[parId];
                 heap[parId] = heap[curId];
                 heap[curId] = temp;
@@ -54,12 +54,12 @@ class Heap {
         cursize--;
         int curid = 0;
         heap[0] = heap[cursize];
-        while(curid * 2 + 1 < cursize) {
-            if(heap[curid] > heap[curid * 2 + 1] && heap[curid] > heap[curid * 2 + 2]) {
+        while (curid * 2 + 1 < cursize) {
+            if (heap[curid] > heap[curid * 2 + 1] && heap[curid] > heap[curid * 2 + 2]) {
                 break;
             }
             int max_id = curid * 2 + 1;
-            if(heap[max_id] < heap[curid * 2 + 2]) {
+            if (heap[max_id] < heap[curid * 2 + 2]) {
                 max_id = curid * 2 + 2;
             }
             int temp = heap[curid];
--- Task_1_1_1/src/test/java/ru/nsu/pereverzev/MainTest.java
+++ Task_1_1_1/src/test/java/ru/nsu/pereverzev/MainTest.java
@@ -1,18 +1,19 @@
 package ru.nsu.pereverzev;
 
-import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import static ru.nsu.pereverzev.Main.heapsort;
 
+import org.junit.jupiter.api.Test;
+
 class MainTest {
     @Test
-    void SampleTest() {
+    void heapsort_test() {
         int[] arr = {1, 5, 1, 2, 9, 4, 7, 6, 3, 2};
         heapsort(arr);
         int len = arr.length;
         int[] sorted = {1, 1, 2, 2, 3, 4, 5, 6, 7, 9};
-        for(int i = 0; i < arr.length; i++) {
+        for (int i = 0; i < arr.length; i++) {
             assertEquals(arr[i], sorted[i]);
         }
     }
