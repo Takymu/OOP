634105f9abe3c25d9822e2678b1d9e6919685671
Takumy <Talumy@duck.com>
2024-09-25 10:22:08 +0700

    added javadoc comments in player class

--- Task_1_1_2/src/main/java/ru/nsu/pereverzev/Player.java
+++ Task_1_1_2/src/main/java/ru/nsu/pereverzev/Player.java
@@ -2,6 +2,8 @@ package ru.nsu.pereverzev;
 
 import java.util.ArrayList;
 
+
+// class that will model player behaviour
 public class Player {
     int blackjack;
     int score;
@@ -13,7 +15,7 @@ public class Player {
         score = 0;
         cardcnt = 0;
     }
-
+    // method returns string with the card names, suits and prices
     public String getCardsList() {
         String s = "";
         int isfirst = 1;
@@ -27,7 +29,7 @@ public class Player {
         }
         return s;
     }
-
+    // method adds card to player, score increments by the value of the card price
     public Card addCardScore(Card card) {
         if (card.getName() == "ace") {
             if (score + card.getPrice() <= Constants.wscore) {
@@ -43,7 +45,7 @@ public class Player {
         pcards.add(card);
         return card;
     }
-
+    // method returns boolean that symbolise, has player blackjack or not
     public boolean isBlackjack() {
         if (score == Constants.wscore && cardcnt == 2) {
             blackjack = 1;
@@ -52,7 +54,7 @@ public class Player {
             return false;
         }
     }
-
+    // returns player's score
     public int getScore() {
         return score;
     }
